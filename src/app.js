ff37f441272a35492d5306851276875a630a6149
Petteri Särkkä <petteri.sarkka@gmail.com>
2019-05-14 17:40:46 +0300

    firts commit, waiting for pictures and text from the customer

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const express = require("express")
+const hbs = require("hbs")
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//Definie paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+//Setup handlebars engine and views location
+app.set("view engine", "hbs")
+app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get("", (req, res) =>{
+  res.render("index", {
+    title: "Etusivu",
+    link_active_kotisivu: "active"
+  })
+})
+
+app.get("/hinnasto", (req, res) =>{
+  res.render("hinnasto", {
+    title: "Hinnasto",
+    link_active_hinnasto: "active"
+  })
+})
+
+app.get("/yhteystiedot", (req, res) =>{
+  res.render("yhteystiedot", {
+    title: "Yhteystiedot",
+    link_active_yhteystiedot: "active"
+  })
+})
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port)
+})
